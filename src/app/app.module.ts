3f4328134c52b79b4fc47505781005b76115443a
dilshanzz <dilshanwijewardhana999@gmail.com>
2024-02-06 03:12:56 +0530

    Adding nav bar and table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './common/nav/nav.component';
+import { ViewAllBooksComponent } from './pages/view-all-books/view-all-books.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    ViewAllBooksComponent
   ],
   imports: [
     BrowserModule,
